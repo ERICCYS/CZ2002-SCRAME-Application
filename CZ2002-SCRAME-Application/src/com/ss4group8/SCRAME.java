64f43b9b62133dd4c00c1d0c031ef58ba8bc983b
Ma Xiao <eric.m.990909@gmail.com>
2018-10-24 17:20:30 +0800

    Update SCRAME Main

--- CZ2002-SCRAME-Application/src/com/ss4group8/SCRAME.java
+++ CZ2002-SCRAME-Application/src/com/ss4group8/SCRAME.java
@@ -4,6 +4,7 @@ public class SCRAME {
     public static void main(String[] args) {
         // If we want to create a new course...
         System.out.println("Hello, welcome to SCRAME!");
+        System.out.println("Bye!");
 
     }
 }
