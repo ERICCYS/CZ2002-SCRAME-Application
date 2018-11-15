e94f318dc6fb2599e2a9d1fd782fa05eacf77fda
mengyan-FU <mfu003@e.ntu.edu.sg>
2018-11-16 00:02:53 +0800

    Auto stash before merge of "master" and "origin/master"

--- CZ2002-SCRAME-Application/src/com/ss4group8/TutorialGroup.java
+++ CZ2002-SCRAME-Application/src/com/ss4group8/TutorialGroup.java
@@ -1,27 +1,6 @@
 package com.ss4group8;
 
 public class TutorialGroup extends Group{
-//    private String groupName;
-//    private int availableVacancies;
-//    private int totalSeats;
-//
-//    public TutorialGroup(String groupName, int availableVacancies, int totalSeats) {
-//        this.groupName = groupName;
-//        this.availableVacancies = availableVacancies;
-//        this.totalSeats = totalSeats;
-//    }
-//
-//    public String getGroupName() { return this.groupName; }
-//
-//    public int getAvailableVacancies() { return this.availableVacancies; }
-//
-//    public int getTotalSeats() {
-//        return totalSeats;
-//    }
-//
-//    public void enrolledIn () {
-//        this.availableVacancies -= 1;
-//    }
 
     public TutorialGroup(String groupName, int availableVacancies, int totalSeats) {
         super(groupName, availableVacancies, totalSeats);
